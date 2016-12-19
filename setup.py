23b08f8663b8e24523dac9466bd8a91414b14032
Christopher Groskopf <chrisgroskopf@gmail.com>
2016-12-19 10:00:30 -0600

    Rev to 0.2.1 for dev.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ install_requires = [
 
 setup(
     name='agate-dbf',
-    version='0.2.0',
+    version='0.2.1',
     description='agate-dbf adds read support for dbf files to agate.',
     long_description=open('README.rst').read(),
     author='Christopher Groskopf',
